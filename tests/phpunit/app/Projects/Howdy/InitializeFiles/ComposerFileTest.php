f9beda00ff901ad7878492a53a709e563568107f
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-10-18 23:15:21 +0700

    Add support to Composer `archive:zip` script (#54)
    This update adds support to the new `archive:zip` script that will be
    used to create archive of the plugin. This command will replace the `npm
    run plugin-zip` command from `wp-scripts` which rely on the npm pack and
    has issues that we can't change. For example, it cannot exclude
    the `README.md` file.

---
+++ tests/phpunit/app/Projects/Howdy/InitializeFiles/ComposerFileTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syntatis\Tests\Projects\Howdy\InitializeFiles;
+
+use PHPUnit\Framework\TestCase;
+use SplFileInfo;
+use Syntatis\Codex\Companion\Projects\Howdy\InitializeFiles\ComposerFile;
+use Syntatis\Tests\WithTemporaryFiles;
+
+use function file_get_contents;
+use function json_decode;
+
+class ComposerFileTest extends TestCase
+{
+	use WithTemporaryFiles;
+
+	private ComposerFile $instance;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		$this->instance = new ComposerFile(
+			[
+				'php_vendor_prefix' => 'PluginName\Vendor',
+				'php_namespace' => 'PluginName',
+				'wp_plugin_name' => 'Plugin Name',
+				'wp_plugin_slug' => 'plugin-name',
+				'wp_plugin_description' => 'This is a plugin description.',
+				'camelcase' => 'pluginName',
+				'kebabcase' => 'plugin-name',
+				'snakecase' => 'plugin_name',
+			],
+			[
+				'php_vendor_prefix' => 'Acme\Plugin\Vendor',
+				'php_namespace' => 'Acme\Plugin',
+				'wp_plugin_name' => 'Acme Plugin',
+				'wp_plugin_slug' => 'acme-plugin',
+				'wp_plugin_description' => 'This is a new plugin description.',
+				'camelcase' => 'acmePlugin',
+				'kebabcase' => 'acme-plugin',
+				'snakecase' => 'acme_plugin',
+			],
+		);
+	}
+
+	public function testArchiveZipScripts(): void
+	{
+		$this->dumpTemporaryFile(
+			'composer.json',
+			<<<'CONTENT'
+			{
+				"scripts": {
+					"archive:zip": "@composer archive --format=zip --file=plugin-name"
+				}
+			}
+			CONTENT,
+		);
+
+		$this->instance->setFile(new SplFileInfo($this->getTemporaryPath('composer.json')));
+		$this->instance->dump();
+
+		$json = json_decode(file_get_contents($this->getTemporaryPath('composer.json')), true);
+
+		$this->assertEquals($json['scripts']['archive:zip'], '@composer archive --format=zip --file=acme-plugin');
+	}
+}
