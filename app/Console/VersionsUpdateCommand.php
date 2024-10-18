b7f4aecd407d8de4e6f98513909de59ffdc904d6
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-10-18 20:36:28 +0700

    Init version updater

--- app/Console/VersionsUpdateCommand.php
+++ app/Console/VersionsUpdateCommand.php
@@ -6,6 +6,9 @@ namespace Syntatis\Codex\Companion\Console;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Syntatis\Codex\Companion\Codex;
+use Syntatis\Codex\Companion\Console\VersionsUpdateCommand\WPVersionsUpdaterProcess;
 
 class VersionsUpdateCommand extends BaseCommand
 {
@@ -18,6 +21,16 @@ class VersionsUpdateCommand extends BaseCommand
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
+		$codex = new Codex($this->projectPath);
+		$style = new SymfonyStyle($input, $output);
+		$type = $codex->getComposer('type');
+
+		if ($type === 'wordpress-plugin') {
+			return (new WPVersionsUpdaterProcess($codex, $style))->execute();
+		}
+
+		$style->warning('Unsupported project type.');
+
 		return 0;
 	}
 }
