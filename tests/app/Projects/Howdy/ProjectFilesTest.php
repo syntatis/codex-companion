c8d58f9fa8025e0d243146eb9d70c4b0999f75cc
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-08-02 23:36:04 +0700

    Include scss and refactor the tests

--- tests/app/Projects/Howdy/ProjectFilesTest.php
+++ tests/app/Projects/Howdy/ProjectFilesTest.php
@@ -9,14 +9,13 @@ use Syntatis\Codex\Companion\Codex;
 use Syntatis\Codex\Companion\Projects\Howdy\ProjectFiles;
 use Syntatis\Tests\WithTemporaryFiles;
 
-use function count;
 use function json_encode;
 
 class ProjectFilesTest extends TestCase
 {
 	use WithTemporaryFiles;
 
-	public function testIgnoredFiles(): void
+	public function testIncludedFiles(): void
 	{
 		$this->dumpTemporaryFile(
 			// Should be included.
@@ -26,11 +25,39 @@ class ProjectFilesTest extends TestCase
 		$this->dumpTemporaryFiles([
 			// Should be included.
 			'src/index.js',
+			'src/style.scss',
 			'foo.php',
 			'bar/hello-world.php',
 			'package.json',
 			'phpcs.xml.dist',
+		]);
 
+		$files = new ProjectFiles(new Codex($this->getTemporaryPath()));
+
+		foreach ($files as $file) {
+			$this->assertContains(
+				$file->getRealPath(),
+				[
+					$this->getTemporaryPath('bar/hello-world.php'),
+					$this->getTemporaryPath('composer.json'),
+					$this->getTemporaryPath('foo.php'),
+					$this->getTemporaryPath('package.json'),
+					$this->getTemporaryPath('phpcs.xml.dist'),
+					$this->getTemporaryPath('src/index.js'),
+					$this->getTemporaryPath('src/style.scss'),
+				],
+			);
+		}
+	}
+
+	public function testIgnoredFiles(): void
+	{
+		$this->dumpTemporaryFile(
+			// Should be included.
+			'composer.json',
+			json_encode(['name' => 'syntatis/howdy']),
+		);
+		$this->dumpTemporaryFiles([
 			// Should be ignored
 			'.editorconfig',
 			'.eslintrc.json',
@@ -65,8 +92,6 @@ class ProjectFilesTest extends TestCase
 				],
 			);
 		}
-
-		$this->assertSame(6, count($files));
 	}
 
 	public function testIgnoredFilesWithCustomScoperOutputDir(): void
@@ -91,7 +116,16 @@ class ProjectFilesTest extends TestCase
 
 		$files = new ProjectFiles(new Codex($this->getTemporaryPath()));
 
-		$this->assertSame(3, count($files));
+		foreach ($files as $file) {
+			$this->assertContains(
+				$file->getRealPath(),
+				[
+					$this->getTemporaryPath('composer.json'),
+					$this->getTemporaryPath('dist/autoload/autoload.php'),
+					$this->getTemporaryPath('dist/autoload/index.js'),
+				],
+			);
+		}
 
 		foreach ($files as $file) {
 			$this->assertNotContains(
